dfdfbdb81feebdb0f00ab495c826e6120b2aed82
elshera dahlan <elshera.dahlan@gmail.com>
2025-02-05 12:42:14 +0800

    exercise 2

---
+++ exercises/exercise2.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const users = require('./users')
+const moment = require('moment')
+
+const server = http.createServer((req, res) => {
+
+    const url = req.url;
+    if(url === '/'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.write("This is the home page")
+    }
+    else if(url === '/about'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/json')
+        res.write(JSON.stringify({
+            status: 'success',
+            message: 'responce success',
+            description: 'exercise #02',
+            date: moment().format('MMMM Do YYYY, h:mm:ss a')
+        }))
+    }
+    else if(url === '/users'){
+        res.statusCode = 200
+        res.write(JSON.stringify(users))
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/json')
+        res.write(JSON.stringify({
+            status: 'failed',
+            message: 'responce failed',
+            description: 'just addition hihihi'
+        }))
+    }
+    res.end()
+})
+
+const hostname = "127.0.0.1"
+const port = 3000
+server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}`))
